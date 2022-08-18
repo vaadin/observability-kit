6e29cc36f192987a31ead30a94ee92e16f6a4df4
Sascha Ißbrücker <sissbruecker@vaadin.com>
2022-08-18 10:05:28 +0200

    Force route update to cover navigation to new route

--- src/main/java/com/vaadin/extension/InstrumentationHelper.java
+++ src/main/java/com/vaadin/extension/InstrumentationHelper.java
@@ -10,8 +10,6 @@ import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.instrumentation.api.instrumenter.LocalRootSpan;
-import io.opentelemetry.instrumentation.api.instrumenter.http.HttpRouteHolder;
-import io.opentelemetry.instrumentation.api.instrumenter.http.HttpRouteSource;
 import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
 
 import java.util.List;
@@ -24,6 +22,12 @@ public class InstrumentationHelper {
     }
 
     public static void updateHttpRoute(UI ui) {
+        Span localRootSpan = LocalRootSpan.fromContextOrNull(Context.current());
+
+        if (localRootSpan == null) {
+            return;
+        }
+
         // Update root span name and http.route attribute to contain route template
         List<HasElement> activeRouterTargetsChain = ui.getInternals().getActiveRouterTargetsChain();
 
@@ -32,18 +36,20 @@ public class InstrumentationHelper {
                 .flatMap(component -> RouteConfiguration.forRegistry(ui.getInternals().getRouter().getRegistry()).getTemplate(component.getClass()));
 
         if (routeTemplate.isPresent()) {
+            // Update root span name to contain the route.
+            // Not using HttpRouteHolder.updateHttpRoute here, as that uses
+            // additional logic that might prevent an update, for example
+            // when the route was already updated by a previous instrumentation.
+            // Updating the root span directly allows us to cover the case
+            // where a request is made against the current route, but the
+            // request actually navigates to a new route, in which case the
+            // root span should show the new route rather than the previous one.
             String route = "/" + routeTemplate.get();
-            Context context = Context.current();
-            HttpRouteHolder.updateHttpRoute(
-                    context,
-                    HttpRouteSource.NESTED_CONTROLLER,
-                    route);
+            localRootSpan.updateName(route);
+            localRootSpan.setAttribute(SemanticAttributes.HTTP_ROUTE, route);
         }
         // Update http.target to contain actual path with params
-        Span localRootSpan = LocalRootSpan.fromContextOrNull(Context.current());
-        if (localRootSpan != null) {
-            String locationPath = "/" + ui.getInternals().getActiveViewLocation().getPath();
-            localRootSpan.setAttribute(SemanticAttributes.HTTP_TARGET, locationPath);
-        }
+        String locationPath = "/" + ui.getInternals().getActiveViewLocation().getPath();
+        localRootSpan.setAttribute(SemanticAttributes.HTTP_TARGET, locationPath);
     }
 }
