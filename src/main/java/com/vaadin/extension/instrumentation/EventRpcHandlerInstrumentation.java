5e3cbf92d6f7ab76acdc389d219e35eb029db8c9
Mikael Grankvist <mikael.grankvist@vaadin.com>
2022-08-19 08:46:14 +0300

    Cleaner output with more attribute usage
    Attributes use `vaadin.`
    Add simple text id if available.

--- src/main/java/com/vaadin/extension/instrumentation/EventRpcHandlerInstrumentation.java
+++ src/main/java/com/vaadin/extension/instrumentation/EventRpcHandlerInstrumentation.java
@@ -76,35 +76,39 @@ public class EventRpcHandlerInstrumentation implements TypeInstrumentation {
             String eventType = jsonObject.getString("event");
 
             final Element element = Element.get(node);
-            span.setAttribute("element-tag", element.getTag());
+            span.setAttribute("vaadin.element.tag", element.getTag());
 
             if (eventType != null) {
-                span.setAttribute("event", eventType);
-                if (element.hasProperty("opened")) {
-                    span.setAttribute("opened", element.getProperty("opened"));
-                    // Note! False means opening as we are checking before the event has happened!
+                span.setAttribute("vaadin.event.type", eventType);
+
+                if (element.hasProperty("opened") && eventType.equals("opened-changed")) {
+                    // Note! False means opening as we are checking before the
+                    // attribute changes for the event have been written!
                     if (element.getProperty("opened", false)) {
-                        eventType = element.getTag() + " opening";
+                        span.setAttribute("vaadin.state.change", "opening");
                     } else {
-                        eventType = element.getTag() + " closing";
+                        span.setAttribute("vaadin.state.change", "closing");
                     }
                 }
                 // If possible add active view class name as an attribute to the span
                 if (node.getOwner() instanceof StateTree) {
-                    span.setAttribute("view",
+                    span.setAttribute("vaadin.view",
                             ((StateTree) node.getOwner()).getUI().getInternals()
                                     .getActiveRouterTargetsChain().get(0)
                                     .getClass().getSimpleName());
                 }
+                String identifier = "";
+                if(element.getText() != null && !element.getText().isEmpty()) {
+                    identifier = String.format("[%s]", element.getText());
+                }
                 // append event type to make span name more descriptive
-                span.updateName(element.getTag() + " :: " + eventType);
+                span.updateName(element.getTag() + identifier + " :: " + eventType);
                 // This will make for instance a click span `vaadin-button :: click`
                 // instead of `EventRpcHandler.handle` which leaves open that what was this about
 
                 // Set the root span name to be the event
                 LocalRootSpan.current().updateName(getActiveRouteTemplate(
-                        ((StateTree) node.getOwner()).getUI()).get() + " "
-                        + eventType);
+                        ((StateTree) node.getOwner()).getUI()).get() + " : event");
             }
         }
 
@@ -114,16 +118,17 @@ public class EventRpcHandlerInstrumentation implements TypeInstrumentation {
                 Throwable throwable,
                 @Advice.Local("otelSpan")
                 Span span) {
-            if (span != null) {
-                if (throwable != null) {
-                    // This will actually mark the span as having an exception which
-                    // shows on the dataUI
-                    span.setStatus(StatusCode.ERROR, throwable.getMessage());
-                    // Add log trace of exception.
-                    span.recordException(throwable);
-                }
-                span.end();
+            if (span == null) {
+                return;
             }
+            if (throwable != null) {
+                // This will actually mark the span as having an exception which
+                // shows on the dataUI
+                span.setStatus(StatusCode.ERROR, throwable.getMessage());
+                // Add log trace of exception.
+                span.recordException(throwable);
+            }
+            span.end();
         }
     }
 
