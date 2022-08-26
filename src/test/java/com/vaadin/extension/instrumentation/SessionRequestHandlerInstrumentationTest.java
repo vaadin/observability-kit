ecdcecdbd1d8578a20cd0dda1c87c9b9d6bb03f0
Matthew Wilson <matthew_w@vaadin.com>
2022-08-26 09:28:05 +0300

    Update with review suggestions

--- src/test/java/com/vaadin/extension/instrumentation/SessionRequestHandlerInstrumentationTest.java
+++ src/test/java/com/vaadin/extension/instrumentation/SessionRequestHandlerInstrumentationTest.java
@@ -4,10 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.vaadin.flow.server.communication.SessionRequestHandler;
 
-import io.opentelemetry.api.trace.StatusCode;
-import io.opentelemetry.sdk.trace.data.EventData;
 import io.opentelemetry.sdk.trace.data.SpanData;
-import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -33,19 +30,6 @@ public class SessionRequestHandlerInstrumentationTest
     }
 
     @Test
-    public void handleRequest_updatesRootSpan() {
-        try (var ignored = withRootContext()) {
-            SessionRequestHandlerInstrumentation.HandleRequestAdvice.onEnter(
-                    sessionRequestHandlerMock, "handleRequest", null, null);
-            SessionRequestHandlerInstrumentation.HandleRequestAdvice
-                    .onExit(null, getCapturedSpan(0), null);
-        }
-
-        SpanData exportedRootSpan = getExportedSpan(1);
-        assertEquals("/", exportedRootSpan.getName());
-    }
-
-    @Test
     public void handleRequestWithException_setsErrorStatus() {
         SessionRequestHandlerInstrumentation.HandleRequestAdvice.onEnter(
                 sessionRequestHandlerMock, "handleRequest", null, null);
@@ -54,14 +38,6 @@ public class SessionRequestHandlerInstrumentationTest
                 .onExit(exception, getCapturedSpan(0), null);
 
         SpanData span = getExportedSpan(0);
-        assertEquals(StatusCode.ERROR, span.getStatus().getStatusCode());
-        assertEquals("test error", span.getStatus().getDescription());
-
-        assertEquals(1, span.getEvents().size());
-        EventData eventData = span.getEvents().get(0);
-        assertEquals(RuntimeException.class.getCanonicalName(), eventData
-                .getAttributes().get(SemanticAttributes.EXCEPTION_TYPE));
-        assertEquals("test error", eventData.getAttributes()
-                .get(SemanticAttributes.EXCEPTION_MESSAGE));
+        assertSpanHasException(span, exception);
     }
 }
