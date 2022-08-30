5a9b5ac297fc5a3ae6517c0b4c6d2d525b5b3a3b
Sascha Ißbrücker <sissbruecker@vaadin.com>
2022-08-30 14:42:12 +0200

    Add trace level to MapSyncRpcHandlerInstrumentation

--- src/main/java/com/vaadin/extension/instrumentation/MapSyncRpcHandlerInstrumentation.java
+++ src/main/java/com/vaadin/extension/instrumentation/MapSyncRpcHandlerInstrumentation.java
@@ -8,9 +8,10 @@ import elemental.json.JsonObject;
 
 import com.vaadin.extension.ElementInstrumentationInfo;
 import com.vaadin.extension.InstrumentationHelper;
+import com.vaadin.extension.conf.Configuration;
+import com.vaadin.extension.conf.TraceLevel;
 import com.vaadin.flow.dom.Element;
 import com.vaadin.flow.internal.StateNode;
-import com.vaadin.flow.server.communication.rpc.MapSyncRpcHandler;
 
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.context.Context;
@@ -51,28 +52,28 @@ public class MapSyncRpcHandlerInstrumentation implements TypeInstrumentation {
     public static class MethodAdvice {
 
         @Advice.OnMethodEnter()
-        public static void onEnter(
-                @Advice.This MapSyncRpcHandler mapSyncRpcHandler,
-                @Advice.Origin("#m") String methodName,
-                @Advice.Argument(0) StateNode node,
+        public static void onEnter(@Advice.Argument(0) StateNode node,
                 @Advice.Argument(1) JsonObject jsonObject,
                 @Advice.Local("otelSpan") Span span,
                 @Advice.Local("otelScope") Scope scope) {
-            final ElementInstrumentationInfo elementInfo = new ElementInstrumentationInfo(
-                    node);
-            final Element element = elementInfo.getElement();
+            if (Configuration.isEnabled(TraceLevel.DEFAULT)) {
+                final ElementInstrumentationInfo elementInfo = new ElementInstrumentationInfo(
+                        node);
+                final Element element = elementInfo.getElement();
 
-            String spanName = "Sync: " + elementInfo.getElementLabel();
-            span = InstrumentationHelper.startSpan(spanName);
-            span.setAttribute("vaadin.element.tag", element.getTag());
-            // If possible add active view class name as an attribute to the
-            // span
-            if (elementInfo.getViewLabel() != null) {
-                span.setAttribute("vaadin.view", elementInfo.getViewLabel());
+                String spanName = "Sync: " + elementInfo.getElementLabel();
+                span = InstrumentationHelper.startSpan(spanName);
+                span.setAttribute("vaadin.element.tag", element.getTag());
+                // If possible add active view class name as an attribute to the
+                // span
+                if (elementInfo.getViewLabel() != null) {
+                    span.setAttribute("vaadin.view",
+                            elementInfo.getViewLabel());
+                }
+
+                Context context = currentContext().with(span);
+                scope = context.makeCurrent();
             }
-
-            Context context = currentContext().with(span);
-            scope = context.makeCurrent();
         }
 
         @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
--- src/test/java/com/vaadin/extension/instrumentation/MapSyncRpcHandlerInstrumentationTest.java
+++ src/test/java/com/vaadin/extension/instrumentation/MapSyncRpcHandlerInstrumentationTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import elemental.json.Json;
 import elemental.json.JsonObject;
 
+import com.vaadin.extension.conf.TraceLevel;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.server.communication.rpc.MapSyncRpcHandler;
@@ -33,9 +34,8 @@ class MapSyncRpcHandlerInstrumentationTest extends AbstractInstrumentationTest {
     public void handleNode_createsSpan() {
         component.getElement().setText("foo");
 
-        MapSyncRpcHandlerInstrumentation.MethodAdvice.onEnter(mapSyncRpcHandler,
-                "handleNode", component.getElement().getNode(), jsonObject,
-                null, null);
+        MapSyncRpcHandlerInstrumentation.MethodAdvice.onEnter(
+                component.getElement().getNode(), jsonObject, null, null);
         MapSyncRpcHandlerInstrumentation.MethodAdvice.onExit(null,
                 getCapturedSpan(0), null);
 
@@ -51,9 +51,8 @@ class MapSyncRpcHandlerInstrumentationTest extends AbstractInstrumentationTest {
 
     @Test
     public void handleNodeWithException_setsErrorStatus() {
-        MapSyncRpcHandlerInstrumentation.MethodAdvice.onEnter(mapSyncRpcHandler,
-                "handleNode", component.getElement().getNode(), jsonObject,
-                null, null);
+        MapSyncRpcHandlerInstrumentation.MethodAdvice.onEnter(
+                component.getElement().getNode(), jsonObject, null, null);
         Exception exception = new RuntimeException("test error");
         MapSyncRpcHandlerInstrumentation.MethodAdvice.onExit(exception,
                 getCapturedSpan(0), null);
@@ -64,5 +63,32 @@ class MapSyncRpcHandlerInstrumentationTest extends AbstractInstrumentationTest {
 
     @Tag("test-component")
     private static class TestComponent extends Component {
+    }
+
+    @Test
+    public void handleNode_respectsTraceLevels() {
+        configureTraceLevel(TraceLevel.MINIMUM);
+        MapSyncRpcHandlerInstrumentation.MethodAdvice.onEnter(
+                component.getElement().getNode(), jsonObject, null, null);
+        MapSyncRpcHandlerInstrumentation.MethodAdvice.onExit(null,
+                getCapturedSpanOrNull(0), null);
+
+        assertEquals(0, getExportedSpanCount());
+
+        configureTraceLevel(TraceLevel.DEFAULT);
+        MapSyncRpcHandlerInstrumentation.MethodAdvice.onEnter(
+                component.getElement().getNode(), jsonObject, null, null);
+        MapSyncRpcHandlerInstrumentation.MethodAdvice.onExit(null,
+                getCapturedSpanOrNull(0), null);
+
+        assertEquals(1, getExportedSpanCount());
+
+        configureTraceLevel(TraceLevel.MAXIMUM);
+        MapSyncRpcHandlerInstrumentation.MethodAdvice.onEnter(
+                component.getElement().getNode(), jsonObject, null, null);
+        MapSyncRpcHandlerInstrumentation.MethodAdvice.onExit(null,
+                getCapturedSpanOrNull(0), null);
+
+        assertEquals(1, getExportedSpanCount());
     }
 }
