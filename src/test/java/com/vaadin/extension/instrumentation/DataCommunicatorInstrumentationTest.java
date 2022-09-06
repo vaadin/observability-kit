fa78dcd402313d7b154e88c6f88894b7a1602323
Sascha Ißbrücker <sissbruecker@vaadin.com>
2022-09-06 11:45:24 +0200

    feat: instrument DataCommunicator (#93)
    Instruments `DataCommunicator` to add a span for every time the data communicator fetches data from a data provider.
    
    This does not cover `HierarchicalDataCommunicator` at the moment.
    
    part of #88

---
+++ src/test/java/com/vaadin/extension/instrumentation/DataCommunicatorInstrumentationTest.java
@@ -0,0 +1,39 @@
+package com.vaadin.extension.instrumentation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.vaadin.flow.data.provider.DataCommunicator;
+import com.vaadin.flow.data.provider.DataProvider;
+
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.sdk.trace.data.SpanData;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class DataCommunicatorInstrumentationTest extends AbstractInstrumentationTest {
+    @Test
+    public void fetchFromDataProvider_createsSpan() {
+        DataProvider dataProvider = Mockito.mock(DataProvider.class);
+        DataCommunicator dataCommunicator = Mockito
+                .mock(DataCommunicator.class);
+        Mockito.when(dataCommunicator.getDataProvider())
+                .thenReturn(dataProvider);
+
+        DataCommunicatorInstrumentation.FetchAdvice.onEnter(dataCommunicator,
+                100, 50, null, null);
+        DataCommunicatorInstrumentation.FetchAdvice.onExit(null,
+                getCapturedSpan(0), null);
+
+        SpanData span = getExportedSpan(0);
+        assertEquals("Data Provider Fetch", span.getName());
+        String dataProviderType = span.getAttributes()
+                .get(AttributeKey.stringKey("vaadin.dataprovider.type"));
+        assertNotNull(dataProviderType);
+        assertTrue(dataProviderType
+                .startsWith("com.vaadin.flow.data.provider.DataProvider"));
+        assertEquals(100, span.getAttributes()
+                .get(AttributeKey.longKey("vaadin.dataprovider.offset")));
+        assertEquals(50, span.getAttributes()
+                .get(AttributeKey.longKey("vaadin.dataprovider.limit")));
+    }
+}
