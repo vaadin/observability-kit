6574839c5852af26483174ef8582bddfcdde100d
Matthew Wilson <matthew_w@vaadin.com>
2022-11-25 11:21:51 +0200

    Add license file and headers

--- src/main/java/com/vaadin/extension/instrumentation/communication/rpc/ReturnChannelHandlerInstrumentation.java
+++ src/main/java/com/vaadin/extension/instrumentation/communication/rpc/ReturnChannelHandlerInstrumentation.java
@@ -1,3 +1,12 @@
+/*-
+ * Copyright (C) 2022 Vaadin Ltd
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ *
+ * See <https://vaadin.com/commercial-license-and-service-terms> for the full
+ * license.
+ */
 package com.vaadin.extension.instrumentation.communication.rpc;
 
 import static io.opentelemetry.javaagent.bootstrap.Java8BytecodeBridge.currentContext;
