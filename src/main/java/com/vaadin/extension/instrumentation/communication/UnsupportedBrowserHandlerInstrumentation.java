050b0dfbc8e63ca2adcfd87f4afea0375883f0e1
Matthew Wilson <98319962+MatthewVaadin@users.noreply.github.com>
2022-11-28 11:50:04 +0200

    chore: Add license file and headers (#139)
    This adds the LICENSE file to the repo root, `<license>` tag to the
    parent POM and license header to all files in the kit.

--- src/main/java/com/vaadin/extension/instrumentation/communication/UnsupportedBrowserHandlerInstrumentation.java
+++ src/main/java/com/vaadin/extension/instrumentation/communication/UnsupportedBrowserHandlerInstrumentation.java
@@ -1,3 +1,12 @@
+/*-
+ * Copyright (C) 2022 Vaadin Ltd
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ *
+ * See <https://vaadin.com/commercial-license-and-service-terms> for the full
+ * license.
+ */
 package com.vaadin.extension.instrumentation.communication;
 
 import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.hasClassesNamed;
