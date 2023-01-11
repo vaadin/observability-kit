b9f8509d6268d6b47a906bab017c8bf5d5e90665
Sergey Vinogradov <mr.vursen@gmail.com>
2023-01-11 15:45:10 +0200

    feat!: Migrate to Jakarta 10 / Servlet 6 (#136)
    Co-authored-by: Giovanni Lovato <giovanni@lova.to>

--- src/test/java/com/vaadin/extension/instrumentation/server/StaticFileServerInstrumentationTest.java
+++ src/test/java/com/vaadin/extension/instrumentation/server/StaticFileServerInstrumentationTest.java
@@ -18,8 +18,8 @@ import org.mockito.Mockito;
 
 import java.time.Instant;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 class StaticFileServerInstrumentationTest extends AbstractInstrumentationTest {
 
