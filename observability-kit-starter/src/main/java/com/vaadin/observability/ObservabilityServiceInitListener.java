c2b77e5693aac834f975fde614daee312d10c0af
Matthew Wilson <matthew_w@vaadin.com>
2023-02-20 07:34:22 +0200

    Move `NpmPackage` annotations to the component

--- observability-kit-starter/src/main/java/com/vaadin/observability/ObservabilityServiceInitListener.java
+++ observability-kit-starter/src/main/java/com/vaadin/observability/ObservabilityServiceInitListener.java
@@ -7,23 +7,9 @@ import org.slf4j.LoggerFactory;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.server.ServiceInitEvent;
 import com.vaadin.flow.server.VaadinServiceInitListener;
 
-@NpmPackage(value = "lit", version = "")
-@NpmPackage(value = "@opentelemetry/sdk-trace-web", version = "1.8.0")
-@NpmPackage(value = "@opentelemetry/instrumentation", version = "0.35.0")
-@NpmPackage(value = "@opentelemetry/instrumentation-document-load",
-        version = "0.31.0")
-@NpmPackage(value = "@opentelemetry/instrumentation-user-interaction",
-        version = "0.32.0")
-@NpmPackage(value = "@opentelemetry/instrumentation-xml-http-request",
-        version = "0.34.0")
-@NpmPackage(value = "@opentelemetry/instrumentation-long-task",
-        version = "0.32.0")
-@NpmPackage(value = "@opentelemetry/exporter-trace-otlp-http",
-        version = "0.35.0")
 public class ObservabilityServiceInitListener
         implements VaadinServiceInitListener {
     private static Logger getLogger() {
