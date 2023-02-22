26fe5377ef94ef4854299f34bc9caf55ab17a700
Marco Collovati <mcollovati@gmail.com>
2023-02-22 12:17:41 +0100

    chore: made disconnectedCallback public (#156)
    IDE complains about disconnectedCallback visibility
    
    TS2415: Class 'ObservabilityClient' incorrectly extends base class
    'LitElement'. Property 'disconnectedCallback' is protected in type
    'ObservabilityClient' but public in type 'LitElement'.

--- observability-kit-starter/src/main/resources/META-INF/resources/frontend/components/observability-client.ts
+++ observability-kit-starter/src/main/resources/META-INF/resources/frontend/components/observability-client.ts
@@ -73,7 +73,7 @@ export class ObservabilityClient extends LitElement {
     });
   }
 
-  protected disconnectedCallback() {
+  disconnectedCallback() {
     super.disconnectedCallback();
 
     if (this.unloadInstrumentations) {
