28efddbac2c517ebae023a5afa84091883e41988
Marco Collovati <mcollovati@gmail.com>
2023-03-06 11:15:35 +0100

    Removed dev bundle and generated files

--- observable-app/src/main/dev-bundle/webapp/VAADIN/build/custom-element-73470d87.js
+++
@@ -1,5 +0,0 @@
-/**
- * @license
- * Copyright 2017 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */const c=s=>t=>typeof t=="function"?((n,e)=>(customElements.define(n,e),e))(s,t):((n,e)=>{const{kind:i,elements:o}=e;return{kind:i,elements:o,finisher(m){customElements.define(n,m)}}})(s,t);export{c as e};
