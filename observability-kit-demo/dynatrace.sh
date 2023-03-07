d637eb6b4c4c62f07274caf827373f96eec7efdd
Marco Collovati <mcollovati@gmail.com>
2023-03-07 09:55:04 +0100

    clean up

--- observability-kit-demo/dynatrace.sh
+++
@@ -1,2 +0,0 @@
-export MAVEN_OPTS="-javaagent:$1 -Dotel.javaagent.configuration-file=dynatrace.properties"
-mvn spring-boot:run -Pproduction
--- observability-kit-demo/newrelic.sh
+++
@@ -1,2 +0,0 @@
-mvn clean package -Pproduction
-java -javaagent:"$1" -Dotel.javaagent.configuration-file=newrelic.properties -jar target/observe-1.0-SNAPSHOT.jar
