322e9d2e32a3bf62c7be98582e766bcf9a01e5aa
Matthew Wilson <matthew_w@vaadin.com>
2023-05-05 12:03:32 +0300

    Add Hilla-specific instrumentation

---
+++ observability-kit-agent/src/main/java/com/vaadin/extension/instrumentation/hilla/PushMessageHandlerInstrumentation.java
@@ -0,0 +1,76 @@
+/*-
+ * Copyright (C) 2023 Vaadin Ltd
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ *
+ * See <https://vaadin.com/commercial-license-and-service-terms> for the full
+ * license.
+ */
+package com.vaadin.extension.instrumentation.hilla;
+
+import dev.hilla.push.messages.fromclient.SubscribeMessage;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
+import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+
+import com.vaadin.extension.InstrumentationHelper;
+import com.vaadin.extension.conf.Configuration;
+import com.vaadin.extension.conf.TraceLevel;
+
+import static io.opentelemetry.javaagent.bootstrap.Java8BytecodeBridge.currentContext;
+import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.hasClassesNamed;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+
+public class PushMessageHandlerInstrumentation implements TypeInstrumentation {
+    @Override
+    public ElementMatcher<ClassLoader> classLoaderOptimization() {
+        return hasClassesNamed("dev.hilla.push.PushMessageHandler");
+    }
+
+    @Override
+    public ElementMatcher<TypeDescription> typeMatcher() {
+        return named("dev.hilla.push.PushMessageHandler");
+    }
+
+    @Override
+    public void transform(TypeTransformer transformer) {
+        transformer.applyAdviceToMethod(named("handleBrowserSubscribe"),
+                this.getClass().getName() + "$SubscribeAdvice");
+    }
+
+    public static class SubscribeAdvice {
+        @Advice.OnMethodEnter()
+        public static void onEnter(@Advice.Argument(0) String connectionId,
+                @Advice.Argument(1) SubscribeMessage message,
+                @Advice.Local("otelSpan") Span span,
+                @Advice.Local("otelScope") Scope scope) {
+            if (Configuration.isEnabled(TraceLevel.DEFAULT)) {
+                String spanName = "Message: Browser subscribe";
+                span = InstrumentationHelper.startSpan(spanName);
+                span.setAttribute("hilla.connection.id", connectionId);
+                span.setAttribute("hilla.flux.id", message.getId());
+                span.setAttribute("hilla.endpoint.name",
+                        message.getEndpointName());
+                span.setAttribute("hilla.method.name", message.getMethodName());
+
+                Context context = currentContext().with(span);
+                scope = context.makeCurrent();
+            }
+        }
+
+        @Advice.OnMethodExit(onThrowable = Throwable.class,
+                suppress = Throwable.class)
+        public static void onExit(@Advice.Thrown Throwable throwable,
+                @Advice.Local("otelSpan") Span span,
+                @Advice.Local("otelScope") Scope scope) {
+            InstrumentationHelper.endSpan(span, throwable, scope);
+        }
+
+    }
+}
