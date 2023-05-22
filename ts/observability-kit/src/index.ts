9a37d111f9265d8ccbcc4114f29f1b3d79612c94
Vlad Rindevich <vladrin@vaadin.com>
2023-05-22 20:03:21 +0300

    feat: add Vaadin frontend telemetry

--- ts/observability-kit/src/index.ts
+++ ts/observability-kit/src/index.ts
@@ -1,2 +1,24 @@
 export * from './HillaEndpointExporter.js';
 export * from './init.js';
+
+declare global {
+  interface VaadinRegistration {
+    is: string;
+    version: string;
+  }
+
+  interface Vaadin {
+    registrations?: VaadinRegistration[];
+  }
+
+  interface Window {
+    Vaadin?: Vaadin;
+  }
+}
+
+window.Vaadin ??= {};
+window.Vaadin.registrations ??= [];
+window.Vaadin.registrations.push({
+  is: '@hilla/observability-kit',
+  version: /* updated-by-script */ '0.1.0',
+});
