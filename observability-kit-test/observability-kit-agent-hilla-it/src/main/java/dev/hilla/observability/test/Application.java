d6ddc47e72f04d821cc1171536a2f4f97ce5d5da
Giovanni Lovato <giovanni@lova.to>
2023-06-20 10:58:51 +0200

    Exclude Tomcat dependency to run with Jetty

--- observability-kit-test/observability-kit-agent-hilla-it/src/main/java/dev/hilla/observability/test/Application.java
+++ observability-kit-test/observability-kit-agent-hilla-it/src/main/java/dev/hilla/observability/test/Application.java
@@ -1,10 +1,11 @@
 package dev.hilla.observability.test;
 
-import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.theme.Theme;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
 
 /**
  * The entry point of the Spring Boot application.
@@ -15,7 +16,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @Theme(value = "observability-kit-agent-hilla-it")
-public class Application implements AppShellConfigurator {
+public class Application extends SpringBootServletInitializer
+        implements AppShellConfigurator {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
